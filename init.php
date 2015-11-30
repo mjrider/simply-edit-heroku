e0fcbc58464647f588ee74c5e4fe1fb53ea550f4
Ben Peachey <potherca@gmail.com>
2015-11-30 14:06:21 +0100

    Mostly whitespace fixes.

--- init.php
+++ init.php
@@ -2,11 +2,10 @@
 require_once  __DIR__ . '/vendor/autoload.php';
 
 use Sabre\DAV\Client;
-use League\Flysystem\Filesystem;
-use League\Flysystem\WebDAV\WebDAVAdapter;
-
 use League\Flysystem\Cached\CachedAdapter;
 use League\Flysystem\Cached\Storage\Memory as CacheStore;
+use League\Flysystem\Filesystem;
+use League\Flysystem\WebDAV\WebDAVAdapter;
 
 $webdav = getenv('webdavendpoint');
 if ( isset($webdav) && $webdav ) {
@@ -17,28 +16,27 @@ if ( isset($webdav) && $webdav ) {
 	exit(1);
 }
 
-$url = new \arc\url\Url( $webdav );
+$url = new \arc\url\Url($webdav);
 
 $baseuri = clone $url;
 $baseuri->path = '/';
+
 unset($baseuri->user);
 unset($baseuri->pass);
 unset($baseuri->path);
 
-
 $settings = [
-	'baseUri'  => (string)$baseuri,
-	'userName' => (string)$url->user,
-	'password' => (string)$url->pass,
+	'baseUri'  => (string) $baseuri,
+	'userName' => (string) $url->user,
+	'password' => (string) $url->pass,
 ];
 
 try {
 	error_log('settings:'.json_encode($settings));
-	$client = new Client($settings);
 	error_log('path:'.json_encode($url->path));
+	
+	$client = new Client($settings);
 	$webdavadapter = new WebDAVAdapter($client,$url->path);
-
-	// Create the cache store
 	$cacheStore = new CacheStore();
 
 	// Decorate the adapter
@@ -47,7 +45,9 @@ try {
 	$flysystem = new Filesystem($adapter);
 
 	$contents = $flysystem->listContents('/');
-} catch (Exception $e){
+} catch (\Exception $e){
+	// @TODO: Handle failure with dignity
 }
+
 return $flysystem;
 
